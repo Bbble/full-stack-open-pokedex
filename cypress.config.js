f6276d0f00736c1fe461e991eeeb0f4ff4b66c08
Bbble <bubliki.etc@gmail.com>
2022-11-12 23:15:25 +0400

    #pipeline: fix linter

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+/* eslint no-undef:0 */
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents() {
+      // implement node event listeners here
+    },
+  }
+})
+/* eslint no-undef:0 */
